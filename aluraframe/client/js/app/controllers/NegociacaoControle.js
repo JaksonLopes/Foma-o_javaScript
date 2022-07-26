2368b81c10ba6073e627046116a5fd43af0c0108
jackson <jacksonlopes948@gmail.com>
2022-07-25 22:49:20 -0300

    Criando class de negociação

---
+++ aluraframe/client/js/app/controllers/NegociacaoControle.js
@@ -0,0 +1,18 @@
+class NegociacaoControle{
+    constructor() {
+        let $ = document.querySelector.bind(document);
+        this.inputData = $ ('#data')
+        this.inputQuaqtidade =  $ ('#quantidade')
+        this.inputvalor =  $ ('#valor')
+    }
+
+    adcionar(evento){
+        evento.preventDefault();
+
+
+        console.log(this.inputvalor.value);
+        console.log(this.inputQuaqtidade.value);
+        console.log(this.inputData.value);
+
+    }
+}
